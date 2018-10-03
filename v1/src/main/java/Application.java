e772f68b20a82c4d5a1cd0150897bbb14e79f02c
maksym hromyk <maksym_hromyk@epam.com>
2018-10-03 17:13:51 +0200

    add .gitignore and apply some modifications

--- v1/src/main/java/Application.java
+++ v1/src/main/java/Application.java
@@ -1,19 +1,29 @@
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
+//@Import(MyConfiguration.class)
+//@ComponentScan()
+@SpringBootApplication
 public class Application {
 
     @Value("${name}")
     private String name;
 
+    @Autowired
+    private String message;
+
     @RequestMapping("/")
     public String home(){
-        return "Welcome to "+name;
+        return "Welcome to "+name+" "+message;
     }
 
     public static void main(String[] args){
